9abeda4f180aa364b25783b12c0b99f840f7da1b
Cristian Matias Figueroa <30852169+CristianFigueroa@users.noreply.github.com>
2017-12-03 13:32:29 -0300

    Update HorarioCarrera.java

--- src/Procesos/HorarioCarrera.java
+++ src/Procesos/HorarioCarrera.java
@@ -1,100 +1,93 @@
 package Procesos;
 
-import Archivos.GestorArchivo;
 import java.util.ArrayList;
+
+import Archivos.GestorArchivo;
+import Ventanas.Inicio;
 
 public class HorarioCarrera extends Horario{
 	
-	private int posicion[][];
-	private int cantidadClases;
-	private int cantidadModulos;
-	
+	private ArrayList <Integer> posicion;
+	private ArrayList <Integer> cantidadAparicionPorModulo;
+
 	public HorarioCarrera() {
-            GestorArchivo gestor = new GestorArchivo();
-            super.inputStream = gestor.getInputCarrera();
-            super.xlsxToArray();
+		this.posicion = new ArrayList<Integer>();
+		this.cantidadAparicionPorModulo = new ArrayList<Integer>();
+        GestorArchivo gestor = new GestorArchivo();
+        super.inputStream = gestor.getInputCarrera();
+        super.xlsxToArray();
 	}
 	
-	   private void ocurrencias (String palabraBuscada) { //determina la cantidad de ocurrencias
-		   int count=0;
-		   	for(int y = 0; y<(this.excell.length-1);y++) {
-		   		for(int x=0;x<(this.excell[0].length);x++) {
-		   			if (this.tomarCelda(x,y).contains(palabraBuscada)) {
-		   				count++;
-		   			}
-		   		}
-		   	}
-		   	this.posicion = new int [count][1];
-		   	llenar(palabraBuscada);
-	   }
-	   
-	   private void llenar(String palabraBuscada) {	// guarda las posiciones de las ocurrencias de la busqueda.
-		   int count=0;
-			for(int y = 0 ; y<(this.excell.length);y++) {
-		   		for(int x=0;x<(this.excell[0].length);x++) {
-		   			if (this.tomarCelda(x,y).contains(palabraBuscada)) {
-		   				this.posicion[count][0]=x;
-		   				this.posicion[count][1]=y;
-		   				count++;
-		   			}
-		   		}
-		   	}
-	   }
-	   
-	   private void setCantidadClases(String palabraBuscada, boolean tipoBusqueda) {
-		   System.out.println(this.posicion[0][0]);
-    	   System.out.println(this.posicion[0][1]+5);
-    	   System.out.println(this.tomarCelda(this.posicion[0][0],this.posicion[0][1]+5));
-                   if(tipoBusqueda) {	//si busque por codigo
-			   this.cantidadClases=Integer.parseInt(this.tomarCelda(this.posicion[0][0],this.posicion[0][1]+5));
-			   
-		   }
-		   else {				//si busque por profesor
-			   this.cantidadClases=Integer.parseInt(this.tomarCelda(this.posicion[0][0],this.posicion[0][1]+2));
-		   }
-	   }
-	   
-	   private Asignatura asignaturaEncontrada (String palabraBuscada, boolean tipoBusqueda, int modulo) { //true para busqueda por codigo false para profesor
+	private void ocurrencias (String palabraBuscada) {
+		for (int y=0; y<this.excell.length; y++) {
+			for(int x=1; x<excell[0].length;x++) {
+				if (this.tomarCelda(x,y).contains(palabraBuscada)) {
+					this.posicion.add(x);
+	   			}
+			}
+		}
+	}
+	
+	private void separarAsignaturas (String palabraBuscada) { //true=codigo / false=profesor
+		this.ocurrencias(palabraBuscada);
+		int count=0;
+		int countAux=0;
+		for(int x=0; x<(this.posicion.size())-1;x++) {
+			if(this.tomarCelda(this.posicion.get(x), 0).equals(this.tomarCelda(this.posicion.get(x)+1, 0))) {
+				count++;
+				countAux++;
+			}else {
+				this.cantidadAparicionPorModulo.add(count+1);
+				count=0;
+			}
+		}	
+		this.cantidadAparicionPorModulo.add(this.posicion.size()-countAux);
+	}
+	
 
-		   String profesor;
-		   String codigo;
-		   String nombre;
-		   ArrayList <String> horario = new ArrayList<String>();
-		   ArrayList <String> sala = new ArrayList<String>();
-		   int clase = ((int) Math.round(this.cantidadClases/modulo))-1;
-		   
-		  if(tipoBusqueda) {
-			  profesor = this.tomarCelda				(this.posicion[clase][0],(this.posicion[clase][1])+3);
-			  codigo = this.tomarCelda					(this.posicion[clase][0],(this.posicion[clase][1])+0);
-			  nombre = this.tomarCelda					(this.posicion[clase][0],(this.posicion[clase][1])+1);
-			  for(int i=0; i<this.cantidadClases; i++) {
-				  horario.add(this.tomarCelda			(this.posicion[clase][0],(this.posicion[clase][1])+2));
-				  sala.add(this.tomarCelda				(this.posicion[clase][0],(this.posicion[clase][1])+4));
-			  }
-		  }else {
-			  profesor = this.tomarCelda				(this.posicion[clase][0],(this.posicion[clase][1])+0);
-			  codigo = this.tomarCelda					(this.posicion[clase][0],(this.posicion[clase][1])-3);
-			  nombre = this.tomarCelda					(this.posicion[clase][0],(this.posicion[clase][1])-2);
-			  for(int i=0; i<this.cantidadClases; i++) {
-				  horario.add(this.tomarCelda			(this.posicion[clase][0],(this.posicion[clase][1])-1));
-				  sala.add(this.tomarCelda				(this.posicion[clase][0],(this.posicion[clase][1])+1));  
-			  }		
-		   }
-		  Asignatura asignatura = new Asignatura(profesor, codigo, nombre, horario, sala);
-		  return asignatura;
-	   }
-	   
-	   
-	   
-	   public ArrayList<Asignatura> listaAsignaturas (String palabraBuscada, boolean tipoBusqueda) {
-		  this.ocurrencias(palabraBuscada);
-          this.setCantidadClases(palabraBuscada, tipoBusqueda);
-		  this.cantidadModulos = (this.posicion.length/cantidadClases);
-		  
-		  ArrayList <Asignatura> listaAsignatura = new ArrayList <Asignatura>();
-		   for(int i=0; i<this.cantidadModulos; i++) {
-			  listaAsignatura.add(asignaturaEncontrada(palabraBuscada, tipoBusqueda, i)); 
-		   }
-		  return listaAsignatura;
-	   }
+	public ArrayList<Asignatura> listaAsignaturas (String palabraBuscada){
+		ArrayList <Asignatura> listaAsignatura = new ArrayList <Asignatura>();
+		this.separarAsignaturas(palabraBuscada);
+		for(int i=0; i<this.cantidadAparicionPorModulo.size(); i++) {		//cantidad de asignaturas
+			listaAsignatura.add(asignaturaEncontrada(i, this.cantidadAparicionPorModulo.get(i))); 
+		}
+		return listaAsignatura;
+	}
+	
+	 private Asignatura asignaturaEncontrada (int posicionEntreLosModulos,int cantidadHorarioDeEstaAsignatura) {
+		 Asignatura asignatura;
+		 String profesor;
+		 String codigo;
+		 String nombre;
+		 ArrayList <String> horario = new ArrayList<String>();
+		 ArrayList <String> sala = new ArrayList<String>();
+		 
+		 int posicionInicial=posicionInicial(posicionEntreLosModulos);		//falta determinar eso con un void 
+		 profesor=	this.tomarCelda(posicionInicial, 3);
+		 codigo=	this.tomarCelda(posicionInicial, 0);
+		 nombre=	this.tomarCelda(posicionInicial, 1);
+		 
+		 for(int i=0;i<cantidadHorarioDeEstaAsignatura;i++) {
+			 horario.add(this.tomarCelda(posicionInicial+i, 2));
+			 sala.add(this.tomarCelda(posicionInicial+i, 4));
+		 }
+		 
+		 asignatura = new Asignatura(profesor, codigo, nombre, horario, sala);
+		 return asignatura;
+	 }
+	
+	
+	private int posicionInicial(int posicionEntreLosModulos) {
+		int contador=0;
+		if(posicionEntreLosModulos>0) {
+			for(int j=0; j<posicionEntreLosModulos; j++) {
+				contador=contador+this.cantidadAparicionPorModulo.get(j);
+						}
+		}else {
+			return 0;
+		}
+		return contador;
+	}
+
+
 }
