4cd97b43ae64c9e9fb52783bf531712eebd3e77a
sebasopazo <30939827+sebasopazo@users.noreply.github.com>
2017-12-02 21:31:09 -0300

    Update ResultadoBusqueda.java

--- src/Ventanas/ResultadoBusqueda.java
+++ src/Ventanas/ResultadoBusqueda.java
@@ -1,13 +1,48 @@
 package Ventanas;
-
-import Procesos.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
-public class ResultadoBusqueda extends JFrame
-{
-     private JList resultados;
-     DefaultListModel model= new DefaultListModel();
-     private JButton volver;
-     
 
+public class ResultadoBusqueda extends JFrame implements ActionListener {
+
+    private JList resultados;
+    DefaultListModel model = new DefaultListModel();
+    private JButton butonVolver, botonSeleccionar;
+    GuiBotones regreso;
+    public ResultadoBusqueda() {
+
+        super("Resultado");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 650);
+
+        resultados = new JList();
+        resultados.setBounds(15, 30, 450, 500);
+        this.setLayout(null);
+        this.add(resultados);
+        
+        butonVolver = new JButton("Volver");
+        butonVolver.setBounds(15, 550, 150, 25);
+        this.add(butonVolver);
+        setLayout(null);
+        butonVolver.addActionListener(this);
+       
+        botonSeleccionar = new JButton("Seleccionar");
+        botonSeleccionar.setBounds( 300, 550, 150, 25);
+        this.add(botonSeleccionar);
+        setLayout(null);
+        botonSeleccionar.addActionListener(this);
+        
+        
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if(ae.getSource()== butonVolver){
+            this.setVisible(false);
+            regreso = new GuiBotones();
+            regreso.setVisible(true);
+        }
+    }
 
 }
