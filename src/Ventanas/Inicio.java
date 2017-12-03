d5100fcbf8971b9c04630973ca96436c6c66586d
Rodrigo Esteban Valenzuela Cardenas <30939648+ZatmanUfro@users.noreply.github.com>
2017-12-02 21:26:30 -0300

    Update Inicio.java
    removed unused imports

--- src/Ventanas/Inicio.java
+++ src/Ventanas/Inicio.java
@@ -1,30 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Ventanas;
+
+
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JFrame;
-import javax.swing.JFrame;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.ArrayList;
-import java.util.Enumeration;
 import javax.swing.*;
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.text.AbstractDocument;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.DocumentFilter;
+
+
 
 /**
  *
@@ -368,5 +350,7 @@ public class Inicio  extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent ae) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+}
     
 }
