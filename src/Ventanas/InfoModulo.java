682a3926f20dda06133786ba943d7a66d4552721
Rodrigo Esteban Valenzuela Cardenas <30939648+ZatmanUfro@users.noreply.github.com>
2017-12-03 00:36:19 -0300

    Update InfoModulo.java

--- src/Ventanas/InfoModulo.java
+++ src/Ventanas/InfoModulo.java
@@ -1,37 +1,38 @@
 package Ventanas;
 
+import Procesos.Asignatura;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
-/**
- *
- * @author sebas
- */
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
 public class InfoModulo extends JFrame implements ActionListener {
      private JLabel labelAsignatura;
      private JButton bt1;
      private JTextArea area;
      
-    public InfoModulo(){
+    public InfoModulo(Asignatura obj){
         
     
        super("UfroTrack");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500, 600);
         
-        labelAsignatura = new JLabel("Codigo asignatura:");
-        labelAsignatura.setText("Codigo asignatura:");
+        labelAsignatura = new JLabel();
+        labelAsignatura.setText("Codigo asignatura:  "+obj.getCodigo());
         labelAsignatura.setBounds(40, 10, 200, 25);
         this.add(labelAsignatura);
-        labelAsignatura = new JLabel("Nombre asignatura:");
-        labelAsignatura.setText("Nombre asignatura:");
+        labelAsignatura = new JLabel();
+        labelAsignatura.setText("Nombre asignatura:  "+obj.getNombre());
         labelAsignatura.setBounds(40, 35, 200, 25);
         this.add(labelAsignatura);
-        labelAsignatura = new JLabel("Nombre profesor:");
-        labelAsignatura.setText("Nombre profesor:");
+        labelAsignatura = new JLabel();
+        labelAsignatura.setText("Nombre profesor:  "+obj.getProfesor());
         labelAsignatura.setBounds(40, 60, 200, 25);
         this.add(labelAsignatura);
-        labelAsignatura = new JLabel("Horarios: ");
+        labelAsignatura = new JLabel();
         labelAsignatura.setText("Horarios:");
         labelAsignatura.setBounds(40, 85, 200, 25);
         this.add(labelAsignatura);
@@ -46,6 +47,7 @@ public class InfoModulo extends JFrame implements ActionListener {
         
         area = new JTextArea();
         area.setBounds(40, 120, 400, 400);
+        area.setText(obj.getHorarios());
         this.add(area);
         setLayout(null);
         
@@ -55,4 +57,5 @@ public class InfoModulo extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent ae) {
        
     }
+}
 }
